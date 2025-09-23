1d6c2d517e66f6cee2c226c3f6ba20833f1a9fd5
Miwm64 <mike.dobkes@gmail.com>
2025-09-23 20:47:50 +0300

    Lab1

--- programming/lab1/code/Main.java
+++ programming/lab1/code/Main.java
@@ -2,23 +2,24 @@ import java.util.Arrays;
 import java.util.Random;
 import java.lang.Math;
 
+
 class Main{
 	public static void main(String[] args){
 		// First array
 		int[] w = new int[7];
-		int c = 0;
-		for (int i = 4; i <= 16; i += 2){
-			w[c] = i;
-			c+=1;
-		}
-
+		
+		for (int i = 0, val = 4; val <= 16; ++i, val += 2){
+			w[i] = val;
+		}	
+		
 		// Second array
 		float[] x = new float[12];
+		
 		Random rd = new Random();
-		float min_rand = -6.0f;
-		float max_rand = 9.0f;
+		final float minRand = -6.0f;
+		final float maxRand = 9.0f;
 		for (int i = 0; i < 12; ++i){
-			x[i] = min_rand + rd.nextFloat() * (max_rand-min_rand);			
+			x[i] = minRand + rd.nextFloat() * (maxRand-minRand);			
 		}
 
 		// Third array
@@ -39,22 +40,32 @@ class Main{
 	* w, x mean values from corresponding arrays
 	*/
 	private static double generateMatrixValue(int w, float x){
-		double ans;
+		double res;
 		if (w == 10){
-			ans = java.lang.Math.pow((2 * java.lang.Math.tan(java.lang.Math.pow((x * (1 - x)), 2))), 2);
+			res = java.lang.Math.pow(
+					(2 * java.lang.Math.tan(
+								java.lang.Math.pow(
+									(x * (1 - x)), 2))
+					 ), 2);
 		} else if (w == 8 || w == 14 || w == 16){
-			ans = java.lang.Math.pow(1.0/3.0/(4.0-((java.lang.Math.pow(java.lang.Math.cbrt(x), (((1.0-(2.0/3.0)/(x+1.0)) / java.lang.Math.PI)))))), 3);
+			res = java.lang.Math.pow(1.0/3.0/
+					(4.0-(
+					       java.lang.Math.pow(
+						       java.lang.Math.cbrt(x),
+						       ((1.0-(2.0/3.0)/(x+1.0)) / java.lang.Math.PI)
+							)))
+				       	, 3);
 
 		} else {
-			ans = java.lang.Math.tan(java.lang.Math.pow(java.lang.Math.E, (java.lang.Math.sin(java.lang.Math.cos(x)))));
+			res = java.lang.Math.tan(java.lang.Math.pow(java.lang.Math.E, (java.lang.Math.sin(java.lang.Math.cos(x)))));
 		}
-		return ans;
+		return res;
 	}
 
 	// Print matrix
 	private static void printDoubleMatrixWithPrecision(double[][] matrix, int precision){
 		for (int i = 0; i < matrix.length; ++i){
-				printDoubleArrayWithPrecision(matrix[i], precision);
+			printDoubleArrayWithPrecision(matrix[i], precision);
 		}	
 	}
 	
@@ -65,8 +76,6 @@ class Main{
 			System.out.printf(" %." + precision + "f", arr[i]);
 		}
 		System.out.println("]");
-		
-		//System.out.println(Arrays.toString(arr));
 	}
-
 }
+
